da1e832d8e3fa4cd7a85463866c1000d6d2f5855
Bradley Daniel <bradley@Bradleys-MBP.Home>
2023-11-10 21:19:08 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Python enviroment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+    pythonEnv = pkgs.python311.withPackages (ps:
+      with ps; [
+        python-dotenv
+        jupyter
+        matplotlib
+        plotly
+        tabulate
+        markdown
+        mysql-connector
+        numpy
+        pandas
+        sqlalchemy
+        pymysql
+
+	# Python Enviroment
+	flake8
+	black
+      ]);
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pythonEnv
+        pkgs.mysql
+
+	# python lsp
+	pkgs.nodePackages.pyright
+      ];
+      shellHook = ''
+        # Exports the sql server url to enviroments variable to be used by vim dbui
+        export $(cat .env | xargs)
+      '';
+    };
+  };
+}
